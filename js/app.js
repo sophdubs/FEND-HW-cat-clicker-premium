532b386d1291099c7519b8fc5024b259f56db401
Sophie Dubois <sophiemargueritedubois@gmail.com>
2018-12-28 20:09:23 +1100

    Rough working webpage.
    Still need some fixes:
    -would like to work on the CSS so that the list and image are side by side.
    -For some reason when the page is initiated, it does not display any cat.
    -There might be a bug with the octopus.init() because this is supposed to set the current cat to model.cats[0];

--- js/app.js
+++ js/app.js
@@ -1,21 +1,108 @@
-let clickCountOne = 0;
-let clickCountTwo = 0;
-let catNameOne = "Bob";
-let catNameTwo = "Jim";
-let catPicOne = document.querySelector(".main-image-1");
-let clicksOne = document.querySelector('.number_of_clicks_one');
-let catPicTwo = document.querySelector(".main-image-2");
-let clicksTwo = document.querySelector('.number_of_clicks_two');
-let nameOne = document.querySelector('.cat-name-one');
-let nameTwo = document.querySelector('.cat-name-two');
 
-nameOne.innerText = catNameOne;
-nameTwo.innerText = catNameTwo;
-catPicOne.addEventListener('click', function(){
-    clickCountOne += 1;
-    clicksOne.innerText = clickCountOne;
-}, false);
-catPicTwo.addEventListener('click', function(){
-    clickCountTwo += 1;
-    clicksTwo.innerText = clickCountTwo;
-}, false);
+let model = {
+    currentCat: null,
+    cats: [
+        {
+            clickCount: 0,
+            name: 'Mittens',
+            imgURL: 'img/cat1.jpeg'
+        },
+        {
+            clickCount: 0,
+            name: 'Snowball',
+            imgURL: 'img/cat2.jpg'
+        },
+        {
+            clickCount: 0,
+            name: 'Fluffy',
+            imgURL: 'img/cat3.jpg'
+        },
+        {
+            clickCount: 0,
+            name: 'Cinnamon',
+            imgURL: 'img/cat4.jpg'
+        },
+        {
+            clickCount: 0,
+            name: 'Hershey',
+            imgURL: 'img/cat5.jpeg'
+        }
+    ]
+};
+
+let octopus = {
+    init: function(){
+        model.currentCat = model.cats[0];
+        catListView.init();
+        catView.init();
+    },
+    getCurrentCat: function(){
+        return model.currentCat;
+    },
+    getCats: function(){
+        return model.cats;
+    },
+    setCurrentCat: function(cat){
+        model.currentCat = cat;
+    },
+    incrementCounter: function(){
+        model.currentCat.clickCount++;
+        catView.render();
+    }
+};
+
+
+let catView = {
+    init: function(){
+        this.catElem = document.querySelector('.cat-viewing-area');
+        this.catNameElem = document.querySelector('.cat-name');
+        this.catPictureElem = document.querySelector('.main-image');
+        this.countElem = document.querySelector('.number_of_clicks');
+
+        this.catPictureElem.addEventListener('click', function(){
+            octopus.incrementCounter();
+        });
+
+        this.render;
+
+    },
+    render: function(){
+        var currentCat = octopus.getCurrentCat();
+        this.countElem.textContent = currentCat.clickCount;
+        this.catNameElem.textContent = currentCat.name;
+        this.catPictureElem.src= currentCat.imgURL;
+    }
+};
+
+let catListView = {
+    init: function(){
+        this.catListElem = document.querySelector('.cat-list');
+
+        this.render();
+
+    },
+    render: function(){
+        var cats = octopus.getCats();
+        this.catListElem.innerHTML = '';
+
+        for (var i = 0; i < cats.length; i++){
+            var cat = cats[i];
+
+            var elem = document.createElement('button');
+            elem.textContent = cat.name;
+
+            elem.addEventListener('click', (function(catcopy){
+                return function(){
+                    octopus.setCurrentCat(catcopy);
+                    catView.render();
+
+                };
+            })(cat));
+
+            this.catListElem.appendChild(elem);
+        };
+    }
+};
+
+
+octopus.init();
